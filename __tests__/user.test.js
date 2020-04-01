0371a20ac151a926cd247f63929365f0169e348a
dorjepadma <dorjepadma@gmail.com>
2020-03-31 18:07:04 -0700

    user and user test passed

---
+++ __tests__/user.test.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const User = require('../lib/models/User');
+
+describe('auth routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('signs up a user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ username: 'Jelly Roll', password: 'strawberry Jam', profilePhotoUrl: 'https://picsum.photos/200/300' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          username: 'Jelly Roll',
+          profilePhotoUrl: 'https://picsum.photos/200/300',
+          __v: 0
+        });
+      });
+  });
+  it('logs in a user', async() => {
+    await User.create({ username: 'Jelly Roll', password: 'Strawberry Jam', profilePhotoUrl: 'https://picsum.photos/200/300' });
+
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({ username: 'Jelly Roll', password: 'Strawberry Jam', profilePhotoUrl: 'https://picsum.photos/200/300' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          username: 'Jelly Roll',
+          profilePhotoUrl: 'https://picsum.photos/200/300',
+          __v: 0
+        });
+      });
+  });
+});
