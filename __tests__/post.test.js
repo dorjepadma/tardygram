bf1ba1738afcc320e87746e611fac70889832f2a
dorjepadma <dorjepadma@gmail.com>
2020-04-05 17:09:40 -0700

    Finished with Post test

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -1,9 +1,6 @@
 require('dotenv').config();
 
-const { getPosts, getUser, getAgent, getPost, getComments } = require('../db/data-helpers');
-
-const request = require('supertest');
-const app = require('../lib/app');
+const { getPosts, getUser, getAgent, getPost, } = require('../db/data-helpers');
 
 describe('post routes', () => {
   
@@ -55,4 +52,29 @@ describe('post routes', () => {
         expect(res.body).toEqual(posts);
       });
   });
+  it('updates a post by id', async() => {
+    const user = await getUser({ username: 'vixen' });
+    const post = await getPost({ user: user._id });
+    
+    return getAgent()
+      .patch(`/api/v1/posts/${post._id}`)
+      .send({ caption: 'When you leave your body recite Om Mani Padme Hum, a deity of light will guide you to the pure realms' })
+      .then(res => {
+        expect(res.body).toEqual({
+          ...post,
+          updatedAt: expect.any(String),
+          caption: 'When you leave your body recite Om Mani Padme Hum, a deity of light will guide you to the pure realms'
+        });
+      });
+  });
+  it('deletes a post', async() => {
+    const user = await getUser({ username: 'vixen' });
+    const post = await getPost({ user: user._id }); 
+    return getAgent()
+      .delete(`/api/v1/posts/${post._id}`)
+      .then(res => {
+        expect(res.body).toEqual(post);
+      });
+  });
+
 });
